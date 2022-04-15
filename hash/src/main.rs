21d6a0d4fadda216a3091cd7b33797e33af5ea53
asingingbird.cb <asingingbird.cb@gmail.com>
2022-04-16 00:37:59 +0800

    improve error handling for hash tool

--- hash/src/main.rs
+++ hash/src/main.rs
@@ -1,6 +1,5 @@
-use crate::HashAlgorithm::MD5;
 use clap::{AppSettings, Arg, ArgMatches, Command};
-use sha2::Digest as Sha2Digest;
+use sha2::Digest;
 use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::string::String;
@@ -12,15 +11,32 @@ fn build_app() -> Command<'static> {
         .about("Print string or file checksums.")
         .setting(AppSettings::DeriveDisplayOrder)
         .override_usage("hash --[md5|sha256|blake3] --text <text>\n    hash --[md5|sha256|blake3] --file <path>")
-        .arg(Arg::new("sha256").short('S').long("sha256").help("Compute the hash using sha256 algorithm (Default)."))
-        .arg(Arg::new("md5").short('M').long("md5").help("Compute the hash using md5 algorithm."))
-        .arg(Arg::new("blake3").short('B').long("blake3").help("Compute the hash using blake3 algorithm."))
+        .arg(
+            Arg::new("sha256")
+                .short('S')
+                .long("sha256")
+                .help("Compute the hash using sha256 algorithm (Default)")
+        )
+        .arg(
+            Arg::new("md5")
+                .short('M')
+                .long("md5")
+                .help("Compute the hash using md5 algorithm")
+                .conflicts_with_all(&["sha256", "blake3"])
+        )
+        .arg(
+            Arg::new("blake3")
+                .short('B')
+                .long("blake3")
+                .help("Compute the hash using blake3 algorithm")
+                .conflicts_with_all(&["md5", "sha256"])
+        )
         .arg(
             Arg::new("text")
                 .short('t')
                 .long("text")
                 .value_name("text")
-                .help("Compute the hash of this text. Can be provided multiple times, compute the hash of each text.")
+                .help("Compute the hash of this text. Can be provided multiple times, compute the hash of each text")
                 .takes_value(true)
                 .multiple_occurrences(true)
         )
@@ -29,7 +45,7 @@ fn build_app() -> Command<'static> {
                 .short('f')
                 .long("file")
                 .value_name("file")
-                .help("Compute the hash of this file. Can be provided multiple times, compute the hash of each file.")
+                .help("Compute the hash of this file. Can be provided multiple times, compute the hash of each file")
                 .takes_value(true)
                 .multiple_occurrences(true)
         )
@@ -37,19 +53,19 @@ fn build_app() -> Command<'static> {
             Arg::new("update")
                 .short('u')
                 .long("update")
-                .help("Instead of computing the hash of each text/file, update on each of them, and print the finalized digest.")
+                .help("Instead of computing the hash of each text/file, update on each of them, and print the finalized digest")
         )
         .arg(
             Arg::new("hex")
                 .short('H')
                 .long("hex")
-                .help("Treat the text or file content as hex strings, e.g. \"0x19 0xab 0xcd 0xef\"")
+                .help("Treat the text or file content as hex strings, e.g. '0x19 0xab 0xcd 0xef'")
         )
         .arg(
             Arg::new("quiet")
                 .short('q')
                 .long("quiet")
-                .help("Do not print the text/file, just the hash.")
+                .help("Do not print the text/file, just the hash")
         )
 }
 
@@ -66,17 +82,21 @@ impl<'a> fmt::Display for HexError<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match *self {
             HexError::InvalidHexPrefix { hex } => {
-                write!(f, "Invalid hex prefix {}, should be 0x or 0X", hex)
+                write!(
+                    f,
+                    "Invalid hex prefix '{}', should start with 0x or 0X",
+                    hex
+                )
             }
             HexError::InvalidLength { hex } => {
                 write!(
                     f,
-                    "Invalid string length {}, should be 4, e.g. \"0x12\"",
+                    "Invalid string length '{}', should be 4, e.g. '0x12'",
                     hex
                 )
             }
             HexError::InvalidHexCharacter { ch, hex } => {
-                write!(f, "Invalid character {} in string {}", ch, hex)
+                write!(f, "Invalid character '{}' in string '{}'", ch, hex)
             }
         }
     }
@@ -98,9 +118,23 @@ fn hex_to_byte(hex_string: &str) -> Result<u8, HexError> {
     } else if !hex_string.starts_with("0x") && !hex_string.starts_with("0X") {
         return Err(HexError::InvalidHexPrefix { hex: hex_string });
     }
+
     let mut chars = hex_string.chars().skip(2);
-    let hi = val(chars.next().unwrap(), hex_string)?;
-    let lo = val(chars.next().unwrap(), hex_string)?;
+    let hi = match chars.next() {
+        None => {
+            return Err(HexError::InvalidLength { hex: hex_string });
+        }
+        Some(c) => c,
+    };
+    let hi = val(hi, hex_string)?;
+
+    let lo = match chars.next() {
+        None => {
+            return Err(HexError::InvalidLength { hex: hex_string });
+        }
+        Some(c) => c,
+    };
+    let lo = val(lo, hex_string)?;
 
     Ok(hi * 16 + lo)
 }
@@ -110,7 +144,13 @@ fn hex_to_byte_slice(hex_string: &str) -> Vec<u8> {
         .split(|c: char| c.is_whitespace() || c == ',')
         .map(|s| s.trim())
         .filter(|s| !s.is_empty())
-        .map(|s| hex_to_byte(s).unwrap())
+        .map(|s| match hex_to_byte(s) {
+            Ok(b) => b,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(exitcode::DATAERR);
+            }
+        })
         .collect()
 }
 
@@ -125,11 +165,18 @@ pub enum HashAlgorithm {
     BLAKE3,
 }
 
+impl Default for HashAlgorithm {
+    fn default() -> Self {
+        Self::SHA256
+    }
+}
+
 #[derive(Clone, Debug, Default)]
 pub struct OutputStyle {
     pub entry: String,
     pub len: usize,
     pub entry_type: &'static str,
+    pub algo: HashAlgorithm,
     pub hash: String,
 }
 
@@ -152,6 +199,10 @@ impl OutputStyle {
         self.entry_type = "TEXT";
     }
 
+    pub fn set_algorithm(&mut self, algorithm: HashAlgorithm) {
+        self.algo = algorithm;
+    }
+
     pub fn add_hash(&mut self, hash_str: &str) {
         self.hash = hash_str.to_string();
     }
@@ -160,7 +211,7 @@ impl OutputStyle {
         let etc = if self.len < 40 { "" } else { "..." };
         let surr_line = "=".repeat(80);
         let entry_line = format!("[{} {}] [{}]{}", action, self.entry_type, self.entry, etc);
-        let hash_line = format!("[HASH] [{}]", self.hash);
+        let hash_line = format!("[{:?} HASH] [{}]", self.algo, self.hash);
         format!(
             "{}\n{}\n{}\n{}\n",
             surr_line, entry_line, hash_line, surr_line
@@ -184,7 +235,7 @@ impl HashImpl {
 
     pub fn digest(input: &[u8], algo: HashAlgorithm) -> Vec<u8> {
         match algo {
-            MD5 => Self::md5hash(input),
+            HashAlgorithm::MD5 => Self::md5hash(input),
             HashAlgorithm::SHA256 => Self::sha256hash(input),
             HashAlgorithm::BLAKE3 => Self::blake3hash(input),
         }
@@ -213,8 +264,8 @@ impl HashImpl {
 
 #[derive(Clone, Debug)]
 pub enum HashInput<'a> {
-    TEXT(&'a str),
-    FILE(&'a str),
+    Text(&'a str),
+    File(&'a str),
 }
 
 fn get_inputs(matches: &ArgMatches) -> Vec<HashInput> {
@@ -249,24 +300,24 @@ fn get_inputs(matches: &ArgMatches) -> Vec<HashInput> {
     let mut inputs = Vec::new();
     while i < text_indices.len() && j < file_indices.len() {
         if text_indices[i] < file_indices[j] {
-            inputs.push(HashInput::TEXT(text_values[i]));
+            inputs.push(HashInput::Text(text_values[i]));
             i += 1;
         } else {
-            inputs.push(HashInput::FILE(file_values[j]));
+            inputs.push(HashInput::File(file_values[j]));
             j += 1;
         }
     }
 
     if i < text_indices.len() {
-        inputs.extend(text_values[i..].iter().map(|x| HashInput::TEXT(x)));
+        inputs.extend(text_values[i..].iter().map(|x| HashInput::Text(x)));
     } else if j < file_indices.len() {
-        inputs.extend(file_values[j..].iter().map(|x| HashInput::FILE(x)));
+        inputs.extend(file_values[j..].iter().map(|x| HashInput::File(x)));
     }
 
     inputs
 }
 
-fn compute(matches: &ArgMatches, inputs: &[HashInput]) {
+pub fn compute(matches: &ArgMatches, inputs: &[HashInput]) {
     let algo = if matches.is_present("md5") {
         HashAlgorithm::MD5
     } else if matches.is_present("blake3") {
@@ -283,8 +334,9 @@ fn compute(matches: &ArgMatches, inputs: &[HashInput]) {
 
     for input in inputs.iter() {
         let mut style = OutputStyle::new();
+        style.set_algorithm(algo);
         let input_bytes = match input {
-            HashInput::TEXT(text) => {
+            HashInput::Text(text) => {
                 if !print_hash_only {
                     style.add_text(text);
                 }
@@ -294,14 +346,26 @@ fn compute(matches: &ArgMatches, inputs: &[HashInput]) {
                     text.as_bytes().to_vec()
                 }
             }
-            HashInput::FILE(file) => {
+            HashInput::File(file) => {
                 if !print_hash_only {
                     style.add_file(file);
                 }
                 if hex_input {
-                    hex_to_byte_slice(&std::fs::read_to_string(file).unwrap())
+                    match std::fs::read_to_string(file) {
+                        Ok(s) => hex_to_byte_slice(&s),
+                        Err(err) => {
+                            eprintln!("Cannot read file {}: {}", file, err);
+                            std::process::exit(exitcode::IOERR);
+                        }
+                    }
                 } else {
-                    std::fs::read(file).unwrap()
+                    match std::fs::read(file) {
+                        Ok(v) => v,
+                        Err(err) => {
+                            eprintln!("Cannot read file {}: {}", file, err);
+                            std::process::exit(exitcode::IOERR);
+                        }
+                    }
                 }
             }
         };
